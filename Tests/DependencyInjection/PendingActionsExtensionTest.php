3ca94cb06e05fa21a6110852beb484adb9f06474
Adrien Lochon <alochon@condenast.fr>
2018-04-18 17:54:27 +0200

    Unit testing for Configuration

---
+++ Tests/DependencyInjection/PendingActionsExtensionTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+* This file is part of the PendingActionsBundle.
+*
+* (c) Adrien Lochon <adrien@claviculanox.io>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace ClaviculaNox\PendingActionsBundle\Tests\DependencyInjection;
+
+use ClaviculaNox\ToolboxBundle\DependencyInjection\ToolboxExtension;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Class PendingActionsExtensionTest.
+ */
+class PendingActionsExtensionTest extends KernelTestCase
+{
+    public function testInjection()
+    {
+        $builder = new ContainerBuilder();
+        $params = [
+            'fs_system_cache_path' => "%kernel.root_dir%/../../build/cache/toolboxbundle",
+            'fs_system_cache_path_chmod' => "06644",
+            'fs_system_cache_path_default_ttl' => 3600,
+        ];
+
+        $ext = new ToolboxExtension();
+        $ext->load([
+            'toolbox' => $params,
+        ], $builder);
+
+        foreach ($params as $key => $value)
+        {
+            $param = $builder->getParameter('toolbox.' . $key);
+            $this->assertEquals($param, $value);
+        }
+    }
+}
